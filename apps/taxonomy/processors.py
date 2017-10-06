8ad1c9497376ef56bd91aaa081118cabd9d728b9
Jd Collins <jd@iamjdcollins.com>
2017-10-06 16:09:57 -0600

    translation items cache

--- apps/taxonomy/processors.py
+++ apps/taxonomy/processors.py
@@ -2,6 +2,8 @@ from .models import Language
 from django.core.cache import cache
 
 def translationlinks(request):
-  translation_items = Language.objects.filter(deleted=0).filter(published=1).order_by('tree_id','level','lft','rght','title')
-  translation_items = cache.get_or_set('TRANSLATION_ITEMS', list(translation_items.values()), 86400)
+  translation_query = Language.objects.filter(deleted=0).filter(published=1).order_by('tree_id','level','lft','rght','title')
+  translation_items = cache.get('TRANSLATION_ITEMS',None)
+  if translation_items == None:
+    translation_items = cache.get_or_set('TRANSLATION_ITEMS', list(translation_query.values()), 86400)
   return {'TRANSLATION_ITEMS': translation_items}
