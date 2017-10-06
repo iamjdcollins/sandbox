b9454973dbe33cebe4d745319ba1d79f1c1975b3
Jd Collins <jd@iamjdcollins.com>
2017-10-06 16:20:55 -0600

    test

--- apps/taxonomy/processors.py
+++ apps/taxonomy/processors.py
@@ -5,5 +5,6 @@ def translationlinks(request):
   translation_query = Language.objects.filter(deleted=0).filter(published=1).order_by('tree_id','level','lft','rght','title')
   translation_items = cache.get('TRANSLATION_ITEMS',None)
   if translation_items == None:
+  	pass
     #translation_items = cache.get_or_set('TRANSLATION_ITEMS', list(translation_query.values()), 86400)
   return {'TRANSLATION_ITEMS': translation_items}
