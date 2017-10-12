64d3ee3a13381a0c3037d14a06ad4cf2cb10f807
Jd Collins <jd@iamjdcollins.com>
2017-10-12 09:07:10 -0600

    Fix processors

--- apps/objects/processors.py
+++ apps/objects/processors.py
@@ -1,9 +1,14 @@
-from .models import School
-from django.core.cache import cache
+from .models import Node
+import mptt
 
-# def school(request):
-#   translation_query = School.objects.filter(deleted=0).filter(published=1).order_by('title')
-#   translation_items = cache.get('TRANSLATION_ITEMS',None)
-#   if translation_items == None:
-#     translation_items = cache.get_or_set('TRANSLATION_ITEMS', list(translation_query.values()), 86400)
-#   return {'TRANSLATION_ITEMS': translation_items}
+def breadcrumb(request):
+  breadcrumb = Node.objects.filter(url=request.path).get_ancestors(include_self=True)
+  return {'BREADCRUMB': breadcrumb}
+
+def mainmenu(request):
+  menu_items = Node.objects.filter(menu_item=1).filter(level=0)
+  return {'MENU_ITEMS': menu_items}
+
+def sitestructure(requset):
+  sitestructure = Node.objects.all()
+  return {'SITESTRUCTURE': sitestructure}
