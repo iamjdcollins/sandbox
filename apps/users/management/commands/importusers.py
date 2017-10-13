7cbaebb9c1a844486559b1985cf14a59c6cc33a3
Jd Collins <jd@iamjdcollins.com>
2017-10-13 13:34:55 -0600

    Impliment Handle Method

--- apps/users/management/commands/importusers.py
+++ apps/users/management/commands/importusers.py
@@ -20,16 +20,17 @@ def importUser(item, account):
   obj.save()
 
 class Command(BaseCommand):
-  importuserssvc = System.objects.get(username='importuserssvc')
-  server = Server('slcsd.net', use_ssl=True, get_info=ALL)
-  conn = Connection(server, user="slcsd\jc024987", password="$i2F'MI\?C@UXK]!3Lm", authentication=NTLM)
-  conn.bind()
-  conn.search('OU=WEB,OU=SERVERS,DC=SLCSD,DC=NET', '(&(objectClass=user)(| (memberof:1.2.840.113556.1.4.1941:=CN=USR_SERVERS_WEB_ALL_ADULT_STAFF,OU=WEB,OU=SERVERS,DC=SLCSD,DC=NET)(memberof:1.2.840.113556.1.4.1941:=CN=USR_SLCSD_NONEMPLOYEE,DC=SLCSD,DC=NET)))', attributes=['DisplayName','userPrincipalName','givenName','sn','objectGUID','mail'])
-  for item in conn.entries:
-    importUser(item, importuserssvc)
-  conn.search('OU=DO,DC=SLCSD,DC=NET', '(&(objectClass=user)(| (memberof:1.2.840.113556.1.4.1941:=CN=USR_SERVERS_WEB_ALL_ADULT_STAFF,OU=WEB,OU=SERVERS,DC=SLCSD,DC=NET)(memberof:1.2.840.113556.1.4.1941:=CN=USR_SLCSD_NONEMPLOYEE,DC=SLCSD,DC=NET)))', attributes=['DisplayName','userPrincipalName','givenName','sn','objectGUID','mail'])
-  for item in conn.entries:
-    importUser(item, importuserssvc)
-  conn.search('OU=INFORMATION_SYSTEMS,DC=SLCSD,DC=NET', '(&(objectClass=user)(|(memberof:1.2.840.113556.1.4.1941:=CN=USR_SERVERS_WEB_ALL_ADULT_STAFF,OU=WEB,OU=SERVERS,DC=SLCSD,DC=NET)(memberof:1.2.840.113556.1.4.1941:=CN=USR_SLCSD_NONEMPLOYEE,DC=SLCSD,DC=NET)))', attributes=['DisplayName','userPrincipalName','givenName','sn','objectGUID','mail'])
-  for item in conn.entries:
-    importUser(item, importuserssvc)
+  def handle(self, *args, **options):
+    importuserssvc = System.objects.get(username='importuserssvc')
+    server = Server('slcsd.net', use_ssl=True, get_info=ALL)
+    conn = Connection(server, user="slcsd\jc024987", password="$i2F'MI\?C@UXK]!3Lm", authentication=NTLM)
+    conn.bind()
+    conn.search('OU=WEB,OU=SERVERS,DC=SLCSD,DC=NET', '(&(objectClass=user)(| (memberof:1.2.840.113556.1.4.1941:=CN=USR_SERVERS_WEB_ALL_ADULT_STAFF,OU=WEB,OU=SERVERS,DC=SLCSD,DC=NET)(memberof:1.2.840.113556.1.4.1941:=CN=USR_SLCSD_NONEMPLOYEE,DC=SLCSD,DC=NET)))', attributes=['DisplayName','userPrincipalName','givenName','sn','objectGUID','mail'])
+    for item in conn.entries:
+      importUser(item, importuserssvc)
+    conn.search('OU=DO,DC=SLCSD,DC=NET', '(&(objectClass=user)(| (memberof:1.2.840.113556.1.4.1941:=CN=USR_SERVERS_WEB_ALL_ADULT_STAFF,OU=WEB,OU=SERVERS,DC=SLCSD,DC=NET)(memberof:1.2.840.113556.1.4.1941:=CN=USR_SLCSD_NONEMPLOYEE,DC=SLCSD,DC=NET)))', attributes=['DisplayName','userPrincipalName','givenName','sn','objectGUID','mail'])
+    for item in conn.entries:
+      importUser(item, importuserssvc)
+    conn.search('OU=INFORMATION_SYSTEMS,DC=SLCSD,DC=NET', '(&(objectClass=user)(|(memberof:1.2.840.113556.1.4.1941:=CN=USR_SERVERS_WEB_ALL_ADULT_STAFF,OU=WEB,OU=SERVERS,DC=SLCSD,DC=NET)(memberof:1.2.840.113556.1.4.1941:=CN=USR_SLCSD_NONEMPLOYEE,DC=SLCSD,DC=NET)))', attributes=['DisplayName','userPrincipalName','givenName','sn','objectGUID','mail'])
+    for item in conn.entries:
+      importUser(item, importuserssvc)
