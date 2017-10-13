25d84aeeb4dbbf0df88943af91b2067c99bc4a06
Jd Collins <jd@iamjdcollins.com>
2017-10-13 15:19:58 -0600

    Add School Administrator Type Field

--- apps/directoryentries/models.py
+++ apps/directoryentries/models.py
@@ -1,6 +1,7 @@
 from django.db import models
 from apps.objects.models import DirectoryEntry
 from apps.users.models import Employee
+from apps.taxonomy.models import SchoolAdministratorType
 import apps.common.functions
 
 class SchoolAdministrator(DirectoryEntry):
@@ -9,6 +10,7 @@ class SchoolAdministrator(DirectoryEntry):
 
   title = models.CharField(max_length=200, help_text='')
   employee = models.ForeignKey(Employee, on_delete=models.CASCADE, related_name='directoryenties_schooladministrator_employee')
+  schooladministratortype = models.ForeignKey(SchoolAdministratorType, on_delete=models.CASCADE, related_name='directoryenties_schooladministrator_schooladministratortype')
 
 
   schooladministrator_directoryentry_node = models.OneToOneField(DirectoryEntry, db_column='schooladministrator_directoryentry_node', on_delete=models.CASCADE, parent_link=True,editable=False,)
